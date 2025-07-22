c78b96f9fe1180a0519c076288691092bbe62787
KangBoram <kb6408@naver.com>
2025-07-22 17:32:43 +0900

    feat : 배경 및 구름 추가

---
+++ src/data/roadmapStagedata.js
@@ -0,0 +1,13 @@
+export const STAGE_POSITIONS = [
+  { id: 0, cloud: { top: "85%", left: "42%" }, char: { top: "80%", left: "44%"} },
+  { id: 1, cloud: { top: "70%", left: "22%" }, char: { top: "65%", left: "24%"} },
+  { id: 2, cloud: { top: "67.5%", left: "42%" }, char: { top: "62.5%", left: "44%" } },
+  { id: 3, cloud: { top: "70%", left: "62%" }, char: { top: "65%", left: "64%" } },
+  { id: 4, cloud: { top: "50%", left: "15%" }, char: { top: "45%", left: "17%" } },
+  { id: 5, cloud: { top: "50%", left: "42%" }, char: { top: "45%", left: "44%" } },
+  { id: 6, cloud: { top: "50%", left: "70%" }, char: { top: "45%", left: "72%" } },
+  { id: 7, cloud: { top: "32.5%", left: "21%" }, char: { top: "27.5%", left: "23%" } },
+  { id: 8, cloud: { top: "30%", left: "42%" }, char: { top: "25%", left: "44%" } },
+  { id: 9, cloud: { top: "35%", left: "63%" }, char: { top: "30%", left: "65%" } },
+  { id: 10, cloud: { top: "15%", left: "42%" }, char: { top: "10%", left: "44%" } },
+];
