f9d7eb2bfa6359fa616154039a1c807b4abd260c
KangBoram <kb6408@naver.com>
2025-07-22 18:43:07 +0900

    feat : 구름 비활성화 추가

--- src/component/Cloud.jsx
+++ src/component/Cloud.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import "../roadMap.css";
 
-function Cloud({ stageId, position, isCurrent, isUnlocked, onClick }) {
-  const current = isCurrent ? "current" : "";
-  const unlocked = isUnlocked ? "unlocked" : "";
+const prefix = `src/assets/roadmap/images/`;
+
+function Cloud({ stageId, position, isCompleted, isCurrent, isProgress }) {
+  let imgSrc;
+	let unlocked;
+
+  if (stageId === 1) {
+    imgSrc = "cloud_start.png";
+  } else if (stageId === 11) {
+		if (!isCompleted && !isProgress) {
+			imgSrc = "cloud_finishi_lock.png";
+		} else {
+			imgSrc = "cloud_finishi_unlock.png";
+		}
+  } else if (isCompleted || isProgress) {
+    imgSrc = "cloud1_unlock.png";
+  } else {
+    imgSrc = "cloud1_lock.png";
+  }
+
+	if(!isCompleted && !isProgress) {
+		unlocked = "locked";
+	} else {
+		unlocked = "unlocked";
+	}
+
   const { top, left } = position;
 
-  const cloudClass = `cloud ${current} ${unlocked}`;
+  const cloudClass = `cloud ${stageId} ${unlocked}`;
 
   return (
-    <div
-      className={cloudClass}
-      style={{ top: top, left: left }}
-      onClick={isUnlocked ? onClick : null}
-    >
-      <img src="src/assets/roadmap/images/cloud1_unlock.png" alt={`Cloud ${stageId}`} />
-      {isUnlocked && <div className="glow-effect"></div>}
+    <div className={cloudClass} style={{ top: top, left: left }}>
+      <img src={`${prefix}${imgSrc}`} alt={`Cloud ${stageId}`} />
     </div>
   );
 }
--- src/pages/roadMapPage.jsx
+++ src/pages/roadMapPage.jsx
@@ -1,26 +1,40 @@
 import { useEffect, useState } from "react";
 import { selectMemberRoadmap } from "../api/roadMapApi";
 import "../roadMap.css";
-import Cloud from "../component/Cloud";
+import Cloud from "../component/cloud";
 import { STAGE_POSITIONS } from "../data/roadmapStagedata";
 
 function RoadMap() {
-  const [currentStage, setCurrentStage] = useState(0);
-  const [isMissionComplete, setIsMissionComplete] = useState(false);
+  // 현재 캐릭터가 있는 스테이지
+  const [charPosition, setCharPosition] = useState(0);
 
-  const characterPosition = STAGE_POSITIONS[currentStage].char;
+  // 현재 수주한 미션들
+  const [progressMissions, setProgressMissions] = useState([]);
+
+  // 완료된 미션들
+  const [completedMissions, setCompletedMissions] = useState([]);
+
+  const characterPosition = STAGE_POSITIONS[charPosition].char;
 
   useEffect(() => {
     selectMemberRoadmap().then((res) => {
-      setCurrentStage(res[0].rsId);
-      setIsMissionComplete(res[0].roadComplete);
+      setCharPosition(res.currentCharPosition);
+      setCompletedMissions(res.completedMissions);
+      setProgressMissions(res.progressMissions);
     });
   }, []);
 
   return (
     <div id="roadmap-background">
       {STAGE_POSITIONS.map((pos) => (
-        <Cloud key={pos.id} stageId={pos.id} position={pos.cloud} />
+        <Cloud
+          key={pos.id}
+          stageId={pos.id}
+          position={pos.cloud}
+          isCompleted={completedMissions.includes(pos.id)}
+          isCurrent={charPosition === pos.id}
+          isProgress={progressMissions.includes(pos.id)}
+        />
       ))}
     </div>
   );
