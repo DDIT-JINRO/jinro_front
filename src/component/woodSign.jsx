ea9f96d1a3ce17d653e83783b583434a9e8616f4
KangBoram <kb6408@naver.com>
2025-07-24 14:48:38 +0900

    feat : 완료된 구름에 표지판 생성 및 완료 날짜 표시 기능 추가

---
+++ src/component/woodSign.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../css/roadmap/woodSign.css";
+
+const prefix = `src/assets/roadmap/images/`;
+const signImgSrc = `${prefix}wood_sign.png`;
+
+function WoodSign({ position, text }) {
+  const { top, left } = position;
+
+  return (
+    <div className="wood-sign-container" style={{ top: top, left: left }}>
+      <img src={signImgSrc} alt="Wood Sign" className="wood-sign-image" />
+      <span className="wood-sign-text">{text}</span>
+    </div>
+  );
+}
+
+export default WoodSign;
