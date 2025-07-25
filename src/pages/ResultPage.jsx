b363517fd22f6c86b128c11be8fb8554edd4ae28
KangBoram <kb6408@naver.com>
2025-07-25 10:49:47 +0900

    feat : 로드맵 완성 모달 및 페이지 이동 기능

---
+++ src/pages/ResultPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ResultPage() {
+  return (
+    <div>
+      <h1>로드맵 결과 페이지</h1>
+      <p>여기에 로드맵 완료에 대한 결과와 추가 정보를 표시합니다.</p>
+      {/* TODO: 실제 결과 내용을 여기에 추가하세요 */}
+    </div>
+  );
+}
+
+export default ResultPage;
