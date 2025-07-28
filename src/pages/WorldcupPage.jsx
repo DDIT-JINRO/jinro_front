5bdf9b5df2518b3fd65569bdec3996e4b09175d5
NZM <nzm6846@gmail.com>
2025-07-28 18:34:06 +0900

    chore : 쓰지않는 코드 삭제

--- src/pages/WorldcupPage.jsx
+++ src/pages/WorldcupPage.jsx
@@ -1,25 +1,9 @@
-import {useState} from "react";
 import CategorySelector from "../components/CategorySelector";
-import Tournament from "../components/Tournament";
-import ResultPage from "../components/ResultPage";
 
 export default function WorldcupPage() {
-    const [step, setStep] = useState("select"); // "select" | "tournament" | "result"
-
-
     return (
         <div className="min-h-screen bg-gray-100 flex justify-center items-center p-4">
-            {step === "select" && (
                 <CategorySelector/>
-            )}
-
-            {step === "tournament" && (
-                <Tournament/>
-            )}
-
-            {step === "result" && (
-                <ResultPage/>  // 우승 직업 정보 전달
-            )}
         </div>
     );
 }
