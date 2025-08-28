7e6f24349a25692ea627ecfba9bea9821a3f9edf
NZM <nzm6846@gmail.com>
2025-08-28 17:53:14 +0900

    feat : modal 전역으로 사용 추가

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,7 @@
+// src/router.js
+
 import { createBrowserRouter } from "react-router-dom";
+import { ModalProvider } from "./context/ModalContext.jsx"; //
 import WorldcupPage from "./pages/worldcup/WorldcupPage";
 import ResultPage from "./pages/worldcup/ResultPage";
 import Tournament from "./components/worldcup/Tournament";
@@ -8,43 +11,51 @@ import RoadmapErrorPage from "./pages/roadmap/roadmapErrorPage";
 import AptiTestPage from "./pages/aptiTest/aptitestPage";
 import AptitestResult from "./components/aptiTest/aptiTestResult";
 import MockInterviewPage from "./pages/mockInterview/MockInterviewPage";
+import RootLayout from "./layout/RootLayout.jsx";
 
 const router = createBrowserRouter([
     {
-        path: "/worldcup/tournament",
-        element: <Tournament />,
-    },
-    {
-        path: "/worldcup",
-        element: <WorldcupPage />,
-    },
-    {
-        path: "/worldcup/result",
-        element: <ResultPage />,
-    },
-    {
-        path: "/roadmap",
-        element: <RoadmapPage />,
-    },
-    {
-        path: "/roadmap/results",
-        element: <RoadmapResultPage />,
-    },
-    {
-        path: "/roadmap/error",
-        element: <RoadmapErrorPage />,
-    },
-    {
-        path: "/aptiTest/:qno",
-        element: <AptiTestPage />
-    },
-    {
-        path: "/aptiTest/result",
-        element: <AptitestResult />
-    },
-    {
-        path: "/mock-interview",
-        element: <MockInterviewPage />,
+        // 최상위 경로에 ModalProvider를 배치
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            {
+                path: "/worldcup/tournament",
+                element: <Tournament />,
+            },
+            {
+                path: "/worldcup",
+                element: <WorldcupPage />,
+            },
+            {
+                path: "/worldcup/result",
+                element: <ResultPage />,
+            },
+            {
+                path: "/roadmap",
+                element: <RoadmapPage />,
+            },
+            {
+                path: "/roadmap/results",
+                element: <RoadmapResultPage />,
+            },
+            {
+                path: "/roadmap/error",
+                element: <RoadmapErrorPage />,
+            },
+            {
+                path: "/aptiTest/:qno",
+                element: <AptiTestPage />
+            },
+            {
+                path: "/aptiTest/result",
+                element: <AptitestResult />
+            },
+            {
+                path: "/mock-interview",
+                element: <MockInterviewPage />,
+            },
+        ],
     },
 ]);
 
