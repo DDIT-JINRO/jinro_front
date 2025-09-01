9c5b2f596247d539ec94d50e2c3151607e26ecd1
KangBoram <kb6408@naver.com>
2025-09-01 09:02:21 +0900

    feat : 구름 상태 차이 및 구름 클릭 시 바로가기

--- src/components/roadmap/cloud.jsx
+++ src/components/roadmap/cloud.jsx
@@ -1,5 +1,5 @@
 import "../../css/roadmap/roadmapPage.css";
-import { CLOUD_STATE } from "../../data/roadmapStagedata";
+import { CLOUD_STATE, SHORT_CUT_URL } from "../../data/roadmapStagedata";
 
 const prefix = `/src/assets/roadmap/images/`;
 
@@ -13,9 +13,52 @@ const prefix = `/src/assets/roadmap/images/`;
  * @param {function} onMouseEnter - 마우스가 구름 진입 시 실행될 함수
  * @param {function} onMouseLeave - 마우스가 구름 이탈 시 실행될 함수
  */
-function Cloud({stageId, position, state, onClick, isCurrent, onMouseEnter, onMouseLeave,}) {
+function Cloud({
+  stageId,
+  position,
+  state,
+  onClick,
+  isCurrent,
+  onMouseEnter,
+  onMouseLeave,
+}) {
   // 이미지 링크
   let imgSrc;
+
+  const handleClick = () => {
+    if (isCurrent) {
+      // 현재 캐릭터가 있는 구름이면 바로가기 URL로 이동
+      const shortcutUrl = SHORT_CUT_URL[stageId - 1]; // stageId는 1부터 시작하므로 -1
+      if (shortcutUrl) {
+        const width = 1200;
+        const height = 800;
+
+        if (stageId == 3) {
+          window.resizeTo(width, height);
+          navigate("/worldcup");
+
+          return;
+        }
+
+        const targetUrl = SHORT_CUT_URL[stageId - 1];
+
+        const message = {
+          type: "navigateParent",
+          url: targetUrl,
+        };
+
+        if (window.opener) {
+          window.opener.postMessage(message, shortcutUrl);
+          window.close();
+        } else {
+          console.log("부모 창을 찾을 수 없습니다.");
+        }
+      }
+    } else if (onClick) {
+      // 그렇지 않으면 기존 onClick 함수 실행
+      onClick();
+    }
+  };
 
   // 스테이지 ID와 상태에 따라 이미지 선택
   if (stageId === 1) {
@@ -33,7 +76,7 @@ function Cloud({stageId, position, state, onClick, isCurrent, onMouseEnter, onMo
     if (state === CLOUD_STATE.COMPLETED || state === CLOUD_STATE.PROGRESS) {
       imgSrc = "cloud1_unlock.png";
     } else {
-      imgSrc = "cloud1_lock.png";
+      imgSrc = "cloud2_lock.png";
     }
   }
 
@@ -46,7 +89,7 @@ function Cloud({stageId, position, state, onClick, isCurrent, onMouseEnter, onMo
       className={cloudClass}
       style={{ top: top, left: left }}
       data-stage={stageId}
-      onClick={onClick}
+      onClick={handleClick}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
